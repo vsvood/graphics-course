0d17bded0f113d8b0a2b764c4d1486d5c8c8959b
Vsevolod Kaspirovich <kaspirovich.vm@phystech.edu>
2024-10-22 23:16:51 +0300

    make old code work

---
+++ tasks/local_shadertoy2/App.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <etna/Window.hpp>
+#include <etna/PerFrameCmdMgr.hpp>
+#include <etna/ComputePipeline.hpp>
+#include <etna/Image.hpp>
+#include <etna/GlobalContext.hpp>
+#include <etna/DescriptorSet.hpp>
+
+#include "wsi/OsWindowingManager.hpp"
+
+
+class App
+{
+public:
+  App();
+  ~App();
+
+  void run();
+
+private:
+  void drawFrame();
+
+private:
+  OsWindowingManager windowing;
+  std::unique_ptr<OsWindow> osWindow;
+
+  etna::GlobalContext* context;
+  etna::ComputePipeline pipeline;
+  etna::Image image;
+  etna::DescriptorSet descriptorSet;
+
+  glm::uvec2 resolution;
+  bool useVsync;
+
+  std::unique_ptr<etna::Window> vkWindow;
+  std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
+};
